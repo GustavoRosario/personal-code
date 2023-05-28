5b8c4d2a1830ddc222c53c8da0b4366804b42d11
Gustavo.R <grosario015@gmail.com>
2023-05-28 13:48:24 -0400

    Optional parameter
    adding default value to second parameter(characters)

--- src/tools/tools_str.py
+++ src/tools/tools_str.py
@@ -6,12 +6,12 @@ import random
 
 
 #Generates a string  with random characters from one already supplied and with the specified length.
-def rannewstr(str_length:int,characters:str):
+def rannewstr(str_length:int,characters:str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%$#@'):
     le:str = len(characters)
     new_str:str = ''
     ctr:chr = ''
     i:int = 0
-    
+
     while(i < str_length):
         i = i + 1
         ctr = characters[random.randint(0,le - 1)]   
