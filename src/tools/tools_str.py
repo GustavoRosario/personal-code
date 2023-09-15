a33b1a3403b4d29d85b1cc720715be4c5209ff24
Gustavo.R <grosario015@gmail.com>
2023-09-15 19:42:13 -0400

    Data Type
    Data type change

--- src/tools/tools_str.py
+++ src/tools/tools_str.py
@@ -7,14 +7,14 @@ import random
 
 #Generates a string  with random characters from one already supplied and with the specified length.
 def rannewstr(str_length:int,characters:str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890%$#@'):
-    le:str = len(characters)
+    le:int = len(characters)
     new_str:str = ''
     ctr:chr = ''
     i:int = 0
 
     while(i < str_length):
         i = i + 1
-        ctr = characters[random.randint(0,le - 1)]   
+        ctr = characters[random.randint(0,le - 1)]
         new_str += ctr
         
     return new_str
